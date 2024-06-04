baf06a3147385fccc372e152b1f55c66aaed8c42
Kobina21 <rkaforson@st.ug.edu.gh>
2024-06-04 16:35:56 +0000

    Create Program.cs

---
+++ Grade Calculator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace GradeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Grade Calculator!");
+            Console.Write("Please enter your numerical grade: ");
+            
+            // Reading the input grade from the user
+            int grade = Convert.ToInt32(Console.ReadLine());
+
+            // Determining the corresponding letter grade
+            string letterGrade;
+            if (grade >= 90)
+            {
+                letterGrade = "A";
+            }
+            else if (grade >= 80)
+            {
+                letterGrade = "B";
+            }
+            else if (grade >= 70)
+            {
+                letterGrade = "C";
+            }
+            else if (grade >= 60)
+            {
+                letterGrade = "D";
+            }
+            else
+            {
+                letterGrade = "F";
+            }
+
+            // Displaying the result
+            Console.WriteLine($"Your letter grade is: {letterGrade}");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
