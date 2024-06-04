9dc874e3459944ba983243e94b7456c5ecc9a2a3
Kobina21 <rkaforson@st.ug.edu.gh>
2024-06-04 16:14:17 +0000

    Create Program.cs

---
+++ Triangle Type Identifier/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TriangleTypeIdentifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter side 1: ");
+            int side1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter side 2: ");
+            int side2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter side 3: ");
+            int side3 = Convert.ToInt32(Console.ReadLine());
+
+            if (side1 == side2 && side2 == side3)
+            {
+                Console.WriteLine("Equilateral Triangle");
+            }
+            else if (side1 == side2 || side2 == side3 || side1 == side3)
+            {
+                Console.WriteLine("Isosceles Triangle");
+            }
+            else
+            {
+                Console.WriteLine("Scalene Triangle");
+            }
+        }
+    }
+}
