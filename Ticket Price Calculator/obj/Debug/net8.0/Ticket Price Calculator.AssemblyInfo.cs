68a0ac382dcfd8e316b4e65fd24115f808ecaf39
Kobina21 <rkaforson@st.ug.edu.gh>
2024-06-17 22:02:38 +0000

    Update Ticket Price Calculator.AssemblyInfo.cs

--- Ticket Price Calculator/obj/Debug/net8.0/Ticket Price Calculator.AssemblyInfo.cs
+++ Ticket Price Calculator/obj/Debug/net8.0/Ticket Price Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ticket Price Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14a6730778cc7fb7b9f2b30d089a7e233e1654cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ticket Price Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ticket Price Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
