b8d12890085612a3f7435779c0246c4fbc1a494e
Kobina21 <rkaforson@st.ug.edu.gh>
2025-08-05 07:46:46 -0400

    commit

--- TicketPriceCalculator/obj/Debug/net8.0/TicketPriceCalculator.AssemblyInfo.cs
+++ TicketPriceCalculator/obj/Debug/net8.0/TicketPriceCalculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketPriceCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb43f9c5a01e534d6e89a12481cb5a7e17dab421")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c8d8b0678a581a0ac93e3eac0ebce9964cca487")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketPriceCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketPriceCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
